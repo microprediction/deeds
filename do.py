765af78f9dbe132cdc4928c1fcbb1cd5fd171edd
Peter Cotton <peter.cotton@microprediction.com>
2020-10-24 12:45:52 -0400

    timestamp

--- do.py
+++ do.py
@@ -1,7 +1,10 @@
+import time
 
 def write_something():
+    stamp = time.time.now()
+    print('writing '+stamp)
     with open("my.csv","at") as f:
-        f.write("\n5.3,5.1,3.2")
+        f.write("\n5.3,5.1,"+str(stamp))
 
 
 if __name__=='__main__':
